265a9edb97708f841a6ce62bebe131f977511e2d
Даниил Андреевич Ляхов <s0123188@edu.kubsu.ru>
2018-09-10 23:22:33 +0300

    add reading file class

---
+++ LU_decomposition/LU_decomposition/SLE_input.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "SLE_input.h"
+
+
+SLE_input::SLE_input(const char* patch){
+	
+	file.open(patch);
+
+	file >> n;
+
+	//Заполнение матрицы
+	a.resize(n);
+	for (int i = 0; i < n; i++) {
+		a[i].resize(n);
+		for (int j = 0; j < n; j++) {
+			file >> a[i][j];
+		}
+	}
+
+	//Заполнение столбца свободных членов
+	b.resize(n);
+	for (int i = 0; i < n; i++) {
+		file >> b[i];
+	}
+
+	//Инициализация вектора x
+
+	x.resize(n);
+}
