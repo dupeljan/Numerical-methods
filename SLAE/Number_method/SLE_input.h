463082d9a0c57777b068cd344f3c26b517c1e079
Даниил Андреевич Ляхов <s0123188@edu.kubsu.ru>
2018-09-13 08:10:29 +0300

    VS shitty commit

---
+++ SLAE/Number_method/SLE_input.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <fstream>
+#include <vector>
+
+class SLE_input
+{
+	std::ifstream file;
+	int n;
+	std::vector < std::vector <float> > a;
+	std::vector < float > b, x;
+public:
+	SLE_input(const char* );
+	std::vector < std::vector <float> > getA() { return a; }
+	std::vector < std::vector <float> > getB_X() { return { b, x }; }
+	float getN() { return n; }
+	
+};
+
