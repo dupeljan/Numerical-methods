27c68d51dc049b6ac0923a56ed28b03392b99caf
Даниил Андреевич Ляхов <s0123188@edu.kubsu.ru>
2018-09-20 12:31:31 +0300

    Add pretty file input

--- LU_decomposition/LU_decomposition/SLE_input.cpp
+++ LU_decomposition/LU_decomposition/SLE_input.cpp
@@ -1,29 +1,37 @@
 ﻿#include "pch.h"
 #include "SLE_input.h"
-
+#include <iostream>
+#include <string>
+#include <sstream>
 
 SLE_input::SLE_input(const char* patch){
 	
 	file.open(patch);
+	std::vector < double > data;
+	//Читаем все в x
+	double x;
+	while (file) {
+		file >> x;
+		data.push_back(x);
+	}
+	//Вычисляем n
 
-	file >> n;
-
-	//Заполнение матрицы
+	n = ( sqrt(1 + 4 * data.size()) - 1 ) / 2;
+	
+	int point = 0;
+	
 	a.resize(n);
+	b.resize(n);
+	
 	for (int i = 0; i < n; i++) {
 		a[i].resize(n);
-		for (int j = 0; j < n; j++) {
-			file >> a[i][j];
-		}
+		for (int j = 0; j < n; j++)
+			a[i][j] = data[point + j];
+		point += n;
+		b[i] = data[point];
+		point++;
 	}
-
-	//Заполнение столбца свободных членов
-	b.resize(n);
-	for (int i = 0; i < n; i++) {
-		file >> b[i];
-	}
-
-	//Инициализация вектора x
-
-	x.resize(n);
 }
+
+
+
--- LU_decomposition/LU_decomposition/SLE_input.h
+++ LU_decomposition/LU_decomposition/SLE_input.h
@@ -7,11 +7,16 @@ class SLE_input
 	std::ifstream file;
 	int n;
 	std::vector < std::vector <double> > a;
-	std::vector < double > b, x;
+	std::vector < double > b;
 public:
 	SLE_input(const char* );
 	std::vector < std::vector <double> > getA() { return a; }
-	std::vector < std::vector <double> > getB_X() { return { b, x }; }
+	std::vector <double>  getB() { return b; }
+	std::vector <double>  getX() { 
+		std::vector<double> x;
+		x.resize(n);
+		return x;
+	}
 	int getN() { return n; }
 	
 };
